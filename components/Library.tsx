18f8048509220db23e6216fff38675d8990769c2
adityam0105 <adityamukherji405@gmail.com>
2024-04-29 05:06:58 +0530

    Added upload modal functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,11 +1,23 @@
 "use client"
 
+import useAuthModel from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
+    const authModal= useAuthModel();
+    const uploadModal= useUploadModal();
+    const { user } =useUser();
     const onClick= ()=>{
+      if(!user){
+        return authModal.onOpen();
+      }
+      // TODO : check for subscription
 
+      return uploadModal.onOpen();
     };
   return (
     <div className="flex flex-col">
