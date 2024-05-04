8879c0ac2595ad630b294e542d243f5893171d7d
adityam0105 <adityamukherji405@gmail.com>
2024-05-04 11:03:17 +0530

    Landing page completed

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types";
+
 import React from "react";
 
 interface PageContentProps {
@@ -10,6 +12,7 @@ interface PageContentProps {
 const PageContent: React.FC<PageContentProps> = ({
     songs
 }) => {
+    const onPlay = useOnPlay(songs);
     if(songs.length === 0){
         return(
             <div className="mt-4 text-neutral-400">
@@ -35,7 +38,7 @@ const PageContent: React.FC<PageContentProps> = ({
         {songs.map((item)=>(
             <SongItem 
             key={item.id}
-            onClick= {()=>{}}
+            onClick= {(id:string)=>onPlay(id)}
             data={item}
             />
         ))}
